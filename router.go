c90acca0b73e7a7d181acea006f9a15d5fe205a5
sky_tsai <sky_tsai@mail.chungyo.net>
2023-04-24 10:50:53 +0800

    test

--- router.go
+++ router.go
@@ -1,23 +1,100 @@
 package router
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/skytsai2/middleware"
 	"github.com/skytsai2/user"
 )
 
+type Data struct {
+	Status    string `json:"status"`
+	ErrorCode string `json:"errorCode"`
+	Users     []User `json:"data"`
+}
+
+type User struct {
+	ID   int
+	Name string
+	Tel  string
+}
+
 func Create(router *gin.Engine) {
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong3",
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/index", func(c *gin.Context) {
+
+		bodystr := sendHttpRequest("http://localhost:8080/api/user/list")
+
+		var data Data
+		err := json.Unmarshal([]byte(bodystr), &data)
+		fmt.Println(bodystr)
+		fmt.Println(data)
+		fmt.Println(err)
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"users": data.Users,
 		})
 	})
 
-	router.POST("/user", user.PostUser)
-	router.PUT("/user", user.PutUser)
-	router.DELETE("/user/:userid", user.DelUser)
-	router.GET("/user/:userid", user.GetUser)
+	router.GET("/test", func(c *gin.Context) {
 
+		bodystr := sendHttpRequest("http://localhost:8080/api/user/list")
+
+		var data Data
+		err := json.Unmarshal([]byte(bodystr), &data)
+		fmt.Println(bodystr)
+		fmt.Printf("%s, %s", data.ErrorCode, data.Status)
+		fmt.Println(err)
+	})
+
+	api := router.Group("/api")
+	api.Use(middleware.Auth())
+	{
+		api.GET("/ping", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+
+		api.POST("/user", user.PostUser)
+		api.PUT("/user", user.PutUser)
+		api.DELETE("/user/:userid", user.DelUser)
+		api.GET("/user/:userid", user.GetUser)
+		api.GET("/user/list", user.GetUserList)
+	}
+}
+
+func sendHttpRequest(url string) string {
+
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	req.Header.Add("Key", "123")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	defer resp.Body.Close()
+	var bodystr string
+	if resp.StatusCode == 200 {
+		body, err := io.ReadAll(resp.Body)
+		bodystr = string(body)
+		if err != nil {
+			log.Fatal(err)
+			return ""
+		}
+	} else {
+		return ""
+	}
+
+	return bodystr
 }
